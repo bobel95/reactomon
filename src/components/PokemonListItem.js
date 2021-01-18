d984d885f25906eee7398fe0a7c9471dc951a0b5
Iulian Popa <bobel2007@outlook.com>
2021-01-18 17:54:30 +0200

    Refactor Types.js to a functional component

--- src/components/PokemonListItem.js
+++ src/components/PokemonListItem.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 const PokemonListItem = props => {
-
 
     const pokemonNumber = props.url.match(/\/([^/]+)\/?$/)[1];
     const formattedNumber = `#${pokemonNumber.padStart(3, '0')}`;
@@ -13,12 +12,6 @@ const PokemonListItem = props => {
             <td><Link to={`/detail?id=${pokemonNumber}`}>{props.name}</Link></td>
         </tr>
     )
-
-
-    // getPokemonSprite = url => {
-    //     return axios.get(url)
-    //             .then(res => this.setState({imgUrl:res.data.sprites.front_default}))
-    // }
 }
 
 export default PokemonListItem
--- src/components/Types.js
+++ src/components/Types.js
@@ -1,45 +1,32 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import ListGroupItem from 'react-bootstrap/ListGroupItem';
 
-export class Types extends Component {
+const Types = props => {
 
-    _isMounted = false;
+    const [data, setData] = useState([])
 
-    state = {
-        results: []
-    }
-
-    componentDidMount = () => {
-        this._isMounted = true;
-    
+    useEffect(() => {
         axios.get(`https://pokeapi.co/api/v2/type`)
-            .then(res => this.setState({ results: res.data.results }));
-    }
+            .then(res => setData(res.data.results));
+    }, [])
 
-    componentWillUnmount = () => {
-        this._isMounted = false;
-    }
+    let typeKey = 1;
+    const types = data
+        .map(type => <ListGroupItem key={typeKey++}>{type.name}</ListGroupItem>);
 
-    render() {
-        console.log(this.state.results);
-
-        const types = this.state.results
-                        .map(t => <ListGroupItem>{t.name}</ListGroupItem>);
-
-        return (
-            <Card style={cardStyle}>
-                <Card.Body>
-                    <Card.Title>Types</Card.Title>
-                </Card.Body>
-                <ListGroup className="list-group-flush">
-                    {types}
-                </ListGroup>
-            </Card>
-        )
-    }
+    return (
+        <Card style={cardStyle}>
+            <Card.Body>
+                <Card.Title>Types</Card.Title>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+                {types}
+            </ListGroup>
+        </Card>
+    )
 }
 
 const cardStyle = {
@@ -48,7 +35,5 @@ const cardStyle = {
     textAlign: 'center',
     border: '2px solid black'
 }
-
-
 
 export default Types
