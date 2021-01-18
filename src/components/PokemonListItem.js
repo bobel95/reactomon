2f0e2a8081136f613ca4558932202a7253c67306
Iulian Popa <bobel2007@outlook.com>
2021-01-18 17:47:16 +0200

    Refactor PokemonListItem.js to a functional component

--- src/components/PokemonListItem.js
+++ src/components/PokemonListItem.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-export class PokemonListItem extends Component {
+const PokemonListItem = props => {
 
-    render() {
-        const pokemonNumber = this.props.url.match(/\/([^/]+)\/?$/)[1];
-        const formattedNumber = `#${pokemonNumber.padStart(3, '0')}`;
 
-        return (
-            <tr>
-                <td>{formattedNumber}</td>
-                <td><Link to={`/detail?id=${pokemonNumber}`}>{this.props.name}</Link></td>
-            </tr>
-        )
-    }
+    const pokemonNumber = props.url.match(/\/([^/]+)\/?$/)[1];
+    const formattedNumber = `#${pokemonNumber.padStart(3, '0')}`;
+
+    return (
+        <tr>
+            <td>{formattedNumber}</td>
+            <td><Link to={`/detail?id=${pokemonNumber}`}>{props.name}</Link></td>
+        </tr>
+    )
+
 
     // getPokemonSprite = url => {
     //     return axios.get(url)
