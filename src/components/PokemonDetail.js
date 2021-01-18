450b7e2ad3f8731d40ccbdbdca9549f4e04f68d7
Iulian Popa <bobel2007@outlook.com>
2021-01-18 18:42:33 +0200

    Refactor PokemonDetail.js to a functional component

--- src/components/PokemonDetail.js
+++ src/components/PokemonDetail.js
@@ -1,63 +1,53 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import ListGroupItem from 'react-bootstrap/ListGroupItem';
 import axios from 'axios';
 
-export class PokemonDetail extends Component {
+const PokemonDetail = props => {
 
-    _isMounted = false;
-
-    state = {
-        data: {
-            sprites: {},
-            types: [
-                {
-                    type: {}
-                }
-            ]
-        }
-    }
-
-    render() {
-        console.log(this.state.data.sprites.front_default);
-
-        const { name, sprites: {front_default : sprite}, weight, height } = this.state.data;
-
-        return (
-            <Card style={cardStyle}>
-                <Card.Img variant="top" src={sprite} />
-                <Card.Body>
-                    <Card.Title>{name}</Card.Title>
-                    <Card.Text>
-                    {this.state.data.types[0].type.name}-type pokemon
-                    </Card.Text>
-                </Card.Body>
-                <ListGroup className="list-group-flush">
-                    <ListGroupItem>Weight: {weight}</ListGroupItem>
-                    <ListGroupItem>Height: {height}</ListGroupItem>
-                </ListGroup>
-            </Card>
-        )
-    }
-
-    componentDidMount = () => {
-        this._isMounted = true;
-
-        const id = this.getIdFromParam();
-        axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-            .then(res => this.setState({ data: res.data }));
-    }
-
-    componentWillUnmount = () => {
-        this._isMounted = false;
-    }
-
-    getIdFromParam = () => {
+    const getIdFromParam = () => {
         let search = window.location.search;
         let params = new URLSearchParams(search);
         return params.get('id');
     }
+
+    const [name, setName] = useState('');
+    const [sprite, setSprite] = useState('');
+    const [weight, setWeight] = useState(0);
+    const [height, setHeight] = useState(0);
+    const [type, setTypes] = useState([]);
+
+    useEffect(() => {
+        const id = getIdFromParam();
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+            .then(res => {
+                console.log(res.data);
+                const { name, sprites: {front_default : sprite}, weight, height } = res.data;
+                const type = res.data.types[0].type.name
+                setName(name);
+                setSprite(sprite);
+                setWeight(weight);
+                setHeight(height);
+                setTypes(type);
+            });
+    }, []);
+
+    return (
+        <Card style={cardStyle}>
+            <Card.Img variant="top" src={sprite} />
+            <Card.Body>
+                <Card.Title>{name}</Card.Title>
+                <Card.Text>
+                {type}-type pokemon
+                </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+                <ListGroupItem>Weight: {weight}</ListGroupItem>
+                <ListGroupItem>Height: {height}</ListGroupItem>
+            </ListGroup>
+        </Card>
+    )
 }
 
 const cardStyle = {
